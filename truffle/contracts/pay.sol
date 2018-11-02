d569cbcee9c5090f41a9c7e2ac7333cc7d798cc7
Je-Ming Lin <lin.13k@gmail.com>
2018-11-01 22:37:59 -0700

    change trans into mapping finish fulfill function add various modifiers add return deduction feature add contractOwner for BitClave to manage variables finish key generation function

--- truffle/contracts/pay.sol
+++ truffle/contracts/pay.sol
@@ -1,264 +1,228 @@
 pragma solidity ^0.4.22;
 
 contract Purchase {
+    /*
+        1 equals to 1/1000, ex: 200 equals to 20%
+    */
+    uint private RETURN_DEDUCTION_RATIO = 200;
+    
+    
+    address private contractOwner;
     struct Transaction {
-        string TransNum;
+        bytes32 TransKey;
         address User;
         address SP;
         address Business;
-        // index 0 for user, index 1 for SP, index 3 for Business
-        bool[] Confirmations;
+        bool isBusinessConfirm;
+        bool isUserConfirm;
+        bool isSPConfirm;
         uint UserShare;
         uint SPShare;
+        bool isValue;
     }
-    Transaction[] public trans;
-    // mapping(uint => Transaction) public trans;
-    // User or SP or Business
-    address[] public customers;
+    mapping(bytes32 => Transaction) private trans;
     
-    // Ensure that `msg.value` is an even number.
-    // Division will truncate if it is an odd number.
-    // Check via multiplication that it wasn't an odd number.
     constructor() public payable {
-
+        contractOwner = msg.sender;
+    }
+    
+    modifier OnlyOwner(){
+        require(msg.sender == contractOwner);
+        _;
     }
 
-    modifier condition(bool _condition) {
-        require(_condition);
+    modifier OnlyBusiness(bytes32 transKey) { 
+        
+        require (trans[transKey].Business == msg.sender); 
+        _; 
+    }
+    
+    modifier OnlyUser(bytes32 transKey) { 
+        
+        require (trans[transKey].User == msg.sender); 
+        _; 
+    }
+    
+    modifier OnlySP(bytes32 transKey) { 
+        
+        require (trans[transKey].SP == msg.sender); 
+        _; 
+    }
+    
+    modifier TransExist(bytes32 transKey) {
+        require(trans[transKey].isValue == true);
         _;
     }
     
-    // # 10
-    // This is the combine API for UserVerification SPVerification, BusinessVerification
-    modifier InContract() {
-        bool res = false;
-        for (uint i = 0; i < customers.length; i++) {
-            if (msg.sender == customers[i]) {
-                res = true;
-                break;
-            }
-        }
-        require(res);
-        _;
-    }
-
-
-
-
-    // Not being used yet
-    // Still figuring out how to use events
-
     event Aborted();
     event PurchaseConfirmed();
     event ItemReceived();
     
-   
-
-    
-    // I haven't thought of a good way to generate a transNum, maybe get the blockchain No.
-    function GenerateTransNum() private pure returns (string thatString) {
-        return "0000000";
+    function GetReturnDeductionRatio()
+        public
+        view
+        returns (uint ratio)
+    {
+        return RETURN_DEDUCTION_RATIO;
     }
     
-    // # 7
-    function InitTransaction(uint UserShare, uint SPShare) private returns (string thatString) {
-        string memory tranNum = GenerateTransNum();
+    function AdjustReturnDeductionRatio(uint ratio)
+        public
+        OnlyOwner
+    {
+        RETURN_DEDUCTION_RATIO = ratio;   
+    }
+    
+    function GenerateTransKey(uint salt) 
+        private view 
+        returns (bytes32 TransKey) 
+    {
+        /* 
+        Should I use other parameter beside "now"
+        */
+        return keccak256(abi.encodePacked(msg.sender, now, salt));
+    }
+    
+    function InitTransaction(uint UserShare, uint SPShare) 
+        public 
+        payable
+        returns (bytes32 TransKey) 
+    {
+        /*
+        TODO set the minimum value for UserShare and SPShare?
+        */
         
-        trans.push(Transaction({
-            TransNum : tranNum,
+        /*
+        check if value is enough
+        */
+        require(msg.value >= UserShare + SPShare);
+        
+        /*
+        making sure the key is not duplicated
+        */
+        uint salt = 0;
+        bytes32 transKey = GenerateTransKey(salt);
+        while (true){
+            if (trans[transKey].isValue){
+                salt += 1;
+                transKey = GenerateTransKey(salt);
+            }else{
+                break;
+            }
+        }    
+        
+        trans[transKey] = Transaction({
+            TransKey : transKey,
             Business : msg.sender,
             User : address(0),
             SP : address(0),
-            Confirmations : new bool[](3),
+            isBusinessConfirm : false,
+            isUserConfirm: false,
+            isSPConfirm: false,
             UserShare : UserShare,
-            SPShare : SPShare
-        }));
-        for (uint i = 0; i < 3; i++) {
-            trans[trans.length - 1].Confirmations[i] = false;
-        }
-        customers.push(msg.sender);
-        return tranNum;
+            SPShare : SPShare,
+            isValue: true
+        });
+        return transKey;
     }
     
-    // #8
-    // 1. Assign the target user into the contract 
-    // 2.Check if User is a valid address or not
-    function AssignUser(string transNum, address User)
+    function AssignUser(bytes32 transKey, address User)
         public
-        InContract()
+        TransExist(transKey)
+        OnlyBusiness(transKey)
     {
-        uint index = 0;
-        bool found = false;
-        for (uint i = 0; i < trans.length; i++) {
-            if (keccak256(trans[i].TransNum) == keccak256(transNum)) {
-                index = i;
-                found = true;
-                break;
-            }
-        }
-        if (!found) {
-            revert();
-            // return;
-        }
-        Transaction storage t =  trans[index];
-        if (msg.sender == t.Business) {
-            t.User = User;
-            customers.push(User);
-        }
+        Transaction storage t =  trans[transKey];
+        t.User = User;
+    }
+    
+    function AssignSP(bytes32 transKey, address SP)
+        public
+        TransExist(transKey)
+        OnlyUser(transKey)
+    {
+        Transaction storage t =  trans[transKey];
+        t.SP = SP;
+    }
+    
+    function UserConfirm(bytes32 transKey)
+        public
+        TransExist(transKey)
+        OnlyUser(transKey)
+    {
+        Transaction storage t =  trans[transKey];
+        t.isUserConfirm = true;
+        FulFill(transKey);
+    }
+    
+    function SPConfirm(bytes32 transKey)
+        public
+        TransExist(transKey)
+        OnlySP(transKey)
+    {
+        Transaction storage t =  trans[transKey];
+        t.isSPConfirm = true;
+        FulFill(transKey);
+    }
+    
+    function BusinessConfirm(bytes32 transKey)
+        public
+        TransExist(transKey)
+        OnlyBusiness(transKey)
+    {
+        Transaction storage t =  trans[transKey];
+        t.isBusinessConfirm = true;
+        FulFill(transKey);
+    }
+    
+    function FulFill(bytes32 transKey)
+        private
+        TransExist(transKey)
+    {
         
-            
+        Transaction storage t =  trans[transKey];
+        require(address(this).balance >= t.SPShare + t.UserShare);
+        if (t.isBusinessConfirm && t.isSPConfirm && t.isUserConfirm){
+            t.User.transfer(t.UserShare);
+            t.SP.transfer(t.SPShare);
+            DeleteTrans(transKey);
+        }
     }
     
-    function AssignSP(string transNum, address SP)
+    function UserReject(bytes32 transKey)
         public
-        InContract()
+        TransExist(transKey)
+        OnlyUser(transKey)
     {
-        uint index = 0;
-        bool found = false;
-        for (uint i = 0; i < trans.length; i++) {
-            if (keccak256(trans[i].TransNum) == keccak256(transNum)) {
-                index = i;
-                found = true;
-                break;
-            }
+        Transaction storage t =  trans[transKey];
+        if (t.isSPConfirm){
+            uint spShare = t.SPShare * RETURN_DEDUCTION_RATIO / 1000;
+            t.SP.transfer(spShare);
+            t.Business.transfer(t.SPShare - spShare + t.UserShare);
+        }else{
+            t.Business.transfer(t.SPShare + t.UserShare);
         }
-        if (!found) {
-            revert();
-        }
-        Transaction storage t =  trans[index];
-        if (msg.sender == t.User) {
-            t.User = SP;
-            customers.push(SP);
-        }
-        
-            
+        DeleteTrans(transKey);
     }
-
-    /// Confirm the purchase 
-    function confirmPurchase(string transNum)
+    
+    function SPReject(bytes32 transKey)
         public
-        InContract()
+        TransExist(transKey)
+        OnlySP(transKey)
     {
-        uint index = 0;
-        bool found = false;
-        // Need to check map
-        for (uint i = 0; i < trans.length; i++) {
-            if (keccak256(trans[i].TransNum) == keccak256(transNum)) {
-                index = i;
-                found = true;
-                break;
-            }
+        Transaction storage t =  trans[transKey];
+        if (t.isUserConfirm){
+            uint userShare = t.SPShare * RETURN_DEDUCTION_RATIO / 1000;
+            t.User.transfer(userShare);
+            t.Business.transfer(t.UserShare - userShare + t.SPShare);
+        }else{
+            t.Business.transfer(t.UserShare + t.SPShare);
         }
-        if (!found) {
-            revert();
-        }
-        Transaction storage t =  trans[index];
-        if (msg.sender == t.User) {
-            t.Confirmations[0] = true;
-        } else if (msg.sender == t.SP) {
-            t.Confirmations[1] = true;
-        } else if (msg.sender == t.Business) {
-            t.Confirmations[2] = true;
-        }
-        for (uint j = 0; j < 3; j++) {
-            if (!t.Confirmations[j]) {
-                revert();
-            }
-        }
-        FullFill(index);
-            
+        DeleteTrans(transKey);
     }
     
-    function deleteTrans(uint index) 
-    private
-    {
-        trans[index] = trans[trans.length - 1];
-        delete trans[trans.length - 1];
-        trans.length--;
-    }
-    
-    // Business cancel, only available before user confirmed
-    function Cancel(string transNum)
-        public
-        InContract()
-    {
-        uint index = 0;
-        bool found = false;
-        for (uint i = 0; i < trans.length; i++) {
-            if (keccak256(trans[i].TransNum) == keccak256(transNum)) {
-                index = i;
-                found = true;
-                break;
-            }
-        }
-        if (!found) {
-            revert();
-        }
-        Transaction storage t =  trans[index];
-        if (msg.sender != t.Business || t.Confirmations[0]) {
-            revert();
-        }
-        deleteTrans(index);
-    }
-    
-    // SP cancel, only available before Business confirmed
-    function SPReject(string transNum)
-        public
-        InContract()
-    {
-        uint index = 0;
-        bool found = false;
-        for (uint i = 0; i < trans.length; i++) {
-          // Change the transN uint64
-            if (keccak256(trans[i].TransNum) == keccak256(transNum)) {
-                index = i;
-                found = true;
-                break;
-            }
-        }
-        if (!found) {
-            revert();
-        }
-        Transaction storage t =  trans[index];
-        if (msg.sender != t.SP || t.Confirmations[2]) {
-            revert();
-        }
-        deleteTrans(index);
-    }
-    
-     // User cancel, only available before Business confirmed
-    function UserReject(string transNum)
-        public
-        InContract()
-    {
-        uint index = 0;
-        bool found = false;
-        for (uint i = 0; i < trans.length; i++) {
-            if (keccak256(trans[i].TransNum) == keccak256(transNum)) {
-                index = i;
-                found = true;
-                break;
-            }
-        }
-        if (!found) {
-            revert();
-        }
-        Transaction storage t =  trans[index];
-        if (msg.sender != t.User || t.Confirmations[1] || t.Confirmations[2]) {
-            revert();
-        }
-        deleteTrans(index);
-    }
-    
-     // # 25/26
-    function FullFill(uint TransIndex)
+    function DeleteTrans(bytes32 transKey)
         private
     {
-        Transaction storage t =  trans[TransIndex];
-        // To be implemented 
-        // Pay User and SP the right Partial
-        
+        delete trans[transKey];
     }
-    
-       
 }
